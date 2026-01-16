637486b9247f78e7e76a7cccfc5a2c6b1ba5fabd
Eray1963 <eray.tan16@gmail.com>
2026-01-16 23:28:01 +0300

    a
    a

--- server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const port = process.env.PORT || 3001;
-
-app.listen(port, () => {
-    console.log(`Sunucu port ${port} üzerinde çalışıyor...`);
-});
